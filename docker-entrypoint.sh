80a2596d315154f8db4be88d6dca50f431e6a4bb
Alexander Koehler <alexander.koehler@inovex.de>
2017-07-12 09:40:59 +0200

    fix docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-/go-shizzle/bin/logstash_exporter -exporter.bind_address :8082 -logstash.endpoint http://localhost:8082/_prometheus/metrics &
+/go-shizzle/bin/logstash_exporter -exporter.bind_address :8082 &
 /usr/share/logstash/bin/logstash -f /etc/logstash/logstash.conf
