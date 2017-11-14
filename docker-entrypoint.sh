5f9c8d63c9604f0464a42caebaf0a7b6bc7f902d
Alexander Koehler <alexander.koehler@inovex.de>
2017-11-14 13:26:06 +0100

    use logstash beta6 rc2 with elasticsearch as config system

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-/usr/share/logstash/bin/logstash --path.settings /usr/share/logstash/config --path.config /usr/share/logstash/conf.d/
+/usr/share/logstash/bin/logstash --path.settings /usr/share/logstash/config
